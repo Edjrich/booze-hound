ef1d4ca6ede3d292f5105a312dcb3b93e6600b6c
erich <ericdjrichardson@gmail.com>
2021-06-23 10:57:49 -0400

    cleaned up out of date psuedo and added extra notes for readability

--- src/App.js
+++ src/App.js
@@ -4,100 +4,38 @@ import { useState } from 'react';
 import Footer from './Footer';
 // import CocktailResults from './CocktailResults';
 
-// PSEUDO JUNE 21
-// make some components 
-// start basic styling
-// create a pull request
-
-// first make component for cocktailResults
-
-
-
-
-// add new info from second api info to new state
-// create new comp
-// that comp will be fed info from state
-// (still running api call in App.JS)
-// on click get api data (conditional statement if state has x show y)
-// show item with z index and position fixed
-// on click, close 'pop up'
-
-
-
-// PSEUDO
-// make repo on github
-// Use axios to connect to API
-// filter cocktails by ingredient (eventually multiple)
-
-// allow user input with drop down menu for multiple ingredients
-// use map to create new array of results 
-// display map on page using grid
-
 //STRETCH 
+// sort by multiple ingredients
 // allow user to save recipe and/or ingredients with firebase 
-
-//MORE PSEUDO 
-  // ADD EVERYSTEP
-  // PUT NOTES EVERYWHERE
-
-
-
-
-      // proper pseudo
-// get api key (email sent)
-// get endpoint (in progress)
-// filter by ingredient (i in params)
-// 
-
-
-// create form to accept user input from drop down menu with ingredients
-// change what is in axios with regular function, and pass in variable colleted from form.
-
-
-// try 2nd api call in cocktailComp if time permits! 
-
-
 
 function App() {
 
   const [drink, setDrink] = useState([]);
-  // const [isLoading, setIsLoading] = useState(true)
-  const [firstChoice, setFirstChoice] = useState('lemonade');
+  const [firstChoice, setFirstChoice] = useState('gin');
+  // set default choice to gin
   const [cocktailDetails, setCocktailDetails] = useState([]);
-  // set default
+
 
   const getData = ((event) => {
     event.preventDefault();
 
-    console.log(`my app has mounted`)
-    
+    // console.log(`my app has mounted`)
+
+    // initial API call. displays drinks on to the page to choose from
      axios({
-      // url: 'http://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita',
       url: 'http://www.thecocktaildb.com/api/json/v1/1/filter.php',
-      // url: 'http://www.thecocktaildb.com/api/json/v1/1/lookup.php',
-      // considering separating api, key, and filter
-      // maybe baseUrl for url, as key as variable, and figure out filter?
-      // need to check api urls
+      // filter by ingredient
       method: 'GET',
-      // dataResponse: 'json',
-      // above may not be required. check later
       params: {
-        // key: `523532`,
-        //  key: `1`,
         format: 'json',
         i: firstChoice
-        // i: `11007`
-
         // i allows to search by ingredient if filter is already applied
         
       }
     }).then( (results) =>{
       console.log(results);
       console.log(results.data.drinks)
-      // console.log(results.data.drinks[0].idDrink)
       setDrink(results.data.drinks)
-      // setIsLoading(false);
-
     })
   })
 
@@ -106,39 +44,31 @@ function App() {
     console.log(event.target.value)
   }
 
-  console.log(userChoice);
-  console.log(firstChoice);
-
-
-
-
+  // console.log(userChoice);
+  // console.log(firstChoice);
 
   const moreInfo = (event) => {
     event.preventDefault();
     console.log(event.target.id)
     const imgId = event.target.id
       
+    // second api call. use drinkId on click to get recipe/information for drink
     axios({
       url: 'http://www.thecocktaildb.com/api/json/v1/1/lookup.php',
+      // lookup by ID
       method: 'GET',
-      // dataResponse: 'json',
-      // above may not be required. check later
       params: {
         // key: `523532`,
         format: 'json',
         i: imgId
-    // ID ABOVE IS IS SPECIFIC TO A DRINK ()
-
-        // i allows to search by ingredient if filter is already applied
-
+        //imgId collected on clicking on photo of drink
       }
     }).then((results) => {
-      console.log(results);
-    console.log(results.data.drinks)
+    //   console.log(results);
+    // console.log(results.data.drinks)
     setCocktailDetails(results.data.drinks)
       // setDrink(results)
       // setIsLoading(false);
-
     })
   }
 
@@ -158,8 +88,6 @@ function App() {
       <header className="wrapper">
         <h1>Hello world. Can I buy you a drink?</h1>
         <form onSubmit={getData}>
-            {/* create function and add onSubmit={newFunciton} to form */}
-          {/* <select name="firstIngredient" id="firstIngredient"> */}
           <select onChange={userChoice}>
             <option value='gin'>Gin</option>
             <option value='tequila'>Tequila</option>
@@ -233,68 +161,6 @@ function App() {
 }
 
 export default App;
-
-
-
-
-                            // WOAH MORE PSEDUO CODE
-
-// Here is my muddled understanding of what I have to do
-// sort by ingredient and display title and image(at least) on page
-// this is done by using filter in my axios API call
-// this will give me the id of the drink, which will allow me to search and get information I need!
-// how to display array of drinks filtered by ingredient 
-
-// axios({
-// url: 'http://www.thecocktaildb.com/api/json/v1/1/filter.php',
-//   // considering separating api, key, and filter
-//   // maybe baseUrl for url, as key as variable, and figure out filter?
-//   method: 'GET',
-//     // dataResponse: 'json',
-//     // above may not be required. check later
-//     params: {
-//   format: 'json',
-//     i: 'gin',z
-//         // i allows to search by ingredient if filter is already applied
-        
-//       }
-//     }).then((results) => {
-//       console.log(results);
-//       console.log(results.data.drinks)
-//       console.log(results.data.drinks[0])
-//       console.log(results.data.drinks[0].idDrink)
-// Above will display array of drinks, and then allow me to target idDrink, which I will then use to search by id
-//       // setDrink(results)
-//       // setIsLoading(false);
-//     })
-
-// allow user to click on photo of drink
-// redirect(?) or display information of drink on page
-// this will possibly require another api call?
-// how to display drink info:
-
-// axios({
-//   url: 'http://www.thecocktaildb.com/api/json/v1/1/lookup.php',
-//   // considering separating api, key, and filter
-//   // maybe baseUrl for url, as key as variable, and figure out filter?
-
-//   method: 'GET',
-//   // dataResponse: 'json',
-//   // above may not be required. check later
-//   params: {
-//     // key: `523532`,
-//     format: 'json',
-//     i: `11007`
-// ID ABOVE IS IS SPECIFIC TO A DRINK ()
-
-//     // i allows to search by ingredient if filter is already applied
-
-//   }
-// }).then((results) => {
-//   console.log(results);
-// console.log(results.data.drinks)
-//   // setDrink(results)
-//   // setIsLoading(false);
 
 // })
 
